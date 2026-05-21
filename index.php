d9bfd7f251be91b16ab7eac8f2995c3072db8d06
ian4734 <40573@ma-web.nl>
2026-05-21 10:09:47 +0200

    Dashboard-Duurzaam

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard-Duurzaam</title>
 </head>
 <body>
      <?php include 'index.html';?>
