be07e1fa8a55f90fddfdca2d4dcd76b6b2ae51f3
deon <deontelman50@gmail.com>
2026-05-21 10:40:01 +0200

    update

--- index.php
+++ index.php
@@ -4,18 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard-Duurzaam</title>
+<<<<<<< HEAD
+    
+=======
     <link rel="stylesheet" href="style.css">
+>>>>>>> e3779a68277c0f081904c7b3679ba7eae68ee854
 </head>
 <body>
     <div class="parent">
-    <div class="div1">1</div>
-    <div class="div2">2</div>
-    <div class="div3">3</div>
-    <div class="div4">4</div> 
-    <div class="div5">5</div>
-    <div class="div6">6</div>
-    <div class="div12">12</div>
-    <div class="div13">13</div>
+    <div class="div1">
+    <?php include sidebar.php; ?>
+    </div>
+    <div class="div2">
+        <?php include header.php; ?>
+    </div>
+    <div class="div3">
+        <?php include temprature.php;?>
+    </div>
+    <div class="div4">
+        <?php include sunset-sunrise.php; ?>
+    </div> 
+    <div class="div5">
+        <?php include house-light.php; ?>
+    </div>
+    <div class="div6">
+        <?php include weather.php; ?>
+    </div>
+    <div class="div12">
+        <?php include enegy-opbrengst-zonnenpanelen.php; ?>
+    </div>
+    <div class="div13">
+        <?php include energy-gebruik; ?>
+    </div>
 </div>
 </body>
 </html>
