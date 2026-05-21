e3779a68277c0f081904c7b3679ba7eae68ee854
ian4734 <40573@ma-web.nl>
2026-05-21 10:23:52 +0200

    php sevrver

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="div1">1</div>
     <div class="div2">2</div>
     <div class="div3">3</div>
-    <div class="div4">4</div>
+    <div class="div4">4</div> 
     <div class="div5">5</div>
     <div class="div6">6</div>
     <div class="div12">12</div>
