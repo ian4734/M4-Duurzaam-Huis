273839574b1cc2cbdfb5fe081fc454ddc9f095ab
Kyano <40870@ma-web.nl>
2026-06-01 12:01:31 +0200

    update

--- index.php
+++ index.php
@@ -7,31 +7,6 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="parent">
-        <div class="div1">
-            <?php include "sidebar.php"; ?>
-        </div>
-        <div class="div2">
-            <?php include "header.php"; ?>
-        </div>
-        <div class="div3">
-            <?php include "temprature.php";?>
-        </div>
-        <div class="div4">
-            <?php include "sunset-sunrise.php"; ?>
-        </div> 
-        <div class="div5">
-            <?php include "house-light.php"; ?>
-        </div>
-        <div class="div6">
-            <?php include "weather.php"; ?>
-        </div>
-        <div class="div12">
-            <?php include "enegy-opbrengst-zonnenpanelen.php"; ?>
-        </div>
-        <div class="div13">
-            <?php include 'energy-gebruik.php'; ?>
-        </div>
-    </div>
+    
 </body>
 </html>
