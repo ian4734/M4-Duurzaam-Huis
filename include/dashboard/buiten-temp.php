fc0b6739a47fd15de0a5efb70830ab2f6d2f7090
Kyano <40870@ma-web.nl>
2026-06-08 12:26:56 +0200

    Start van temperatuur

---
+++ include/dashboard/buiten-temp.php
@@ -0,0 +1,9 @@
+<body>
+    <div class="temperature">
+        <h2>Buiten temperatuur</h2>
+        <p>20°C</p>
+        <p>Voelt als 18°C</p>
+        <h2>Binnen temperatuur</h2>
+        <p>22°C</p>
+    </div>
+</body>
