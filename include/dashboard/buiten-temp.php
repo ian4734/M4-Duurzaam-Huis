d7ff9529d275687571d4e06cbf06f4590c499bd7
Kyano <40870@ma-web.nl>
2026-06-11 10:07:52 +0200

    actuele buiten temp done

--- include/dashboard/buiten-temp.php
+++ include/dashboard/buiten-temp.php
@@ -1,8 +1,21 @@
+<?php 
+
+$url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m";
+$curl = curl_init($url);
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+
+$data = json_decode($response, true);
+$temperature = $data['current']['temperature_2m'];
+
+
+?>
+
 <body>
     <div class="temperature">
         <h2>Buiten temperatuur</h2>
-        <p>20°C</p>
-        <p>Voelt als 18°C</p>
+        <p><?php echo $temperature; ?>°C</p>
         <h2>Binnen temperatuur</h2>
         <p>22°C</p>
     </div>
