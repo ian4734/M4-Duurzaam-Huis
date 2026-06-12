2b8eb5d0b345aaa8d01fb3e096826d19bf03184c
Ian <40573@ma-web.nl>
2026-06-12 10:00:45 +0200

    changed from grid to flex and tried to fix mistakes made

--- index.php
+++ index.php
@@ -6,31 +6,30 @@
     <title>Dashboard-Duurzaam</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="buiten-temp.css">
+
 </head>
 <body>
-    <header <div class="darkmode"><?php include 'include/dashboard/Darkmode.php'; ?></div>
-            class="header"> <?php include 'include/header.php'; ?>      
-    </header>
+
+<div class="page-container">
+    <header><?php include 'include/header.php'; ?></header>
 
     <div class="main-wrapper">
         <aside class="sidebar">
             <?php include 'include/dashboard/sidebar.php'; ?>
         </aside>
 
-        <main class="parent">
-            <div class="temperature"><?php include 'include/dashboard/buiten-temp.php'; ?></div>
-            <div class="sunsetsunrise"><?php include 'include/dashboard/sunset-sunrise.php'; ?></div>
-            <div class="weatherforecast"><?php include 'include/dashboard/weatherforecast.php'; ?></div>
-            <div class="zonnepanelen"><?php include 'include/dashboard/zonnepanelen.php'; ?></div>
-            <div class="energie"><?php include 'include/dashboard/energie.php'; ?></div>
-            <div class="lampen"><?php include 'include/dashboard/lampen.php'; ?></div>
-            <div class="parent"></div>
-            </main>
-
+        <main class="dashboard-content">
+            <?php include 'include/dashboard/buiten-temp.php'; ?>
+            <?php include 'include/dashboard/sunset-sunrise.php'; ?>
+            <?php include 'include/dashboard/weatherforecast.php'; ?>
+            <?php include 'include/dashboard/zonnepanelen.php'; ?>
+            <?php include 'include/dashboard/energie.php'; ?>
+            <?php include 'include/dashboard/lampen.php'; ?>
+        </main>
     </div>
-    
-    <footer class="footer"><?php include 'include/footer.php'; ?></div>
+
+    <footer class="footer"><?php include 'include/footer.php'; ?></footer>
+</div>
 </body> 
 </html>
-
 
