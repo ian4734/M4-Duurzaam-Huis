d589eb8466aef9f74bfb30e006b6fb31efe43985
DEON <40478@ma-web.nl>
2026-06-12 09:21:32 +0200

    del grid

--- index.php
+++ index.php
@@ -10,25 +10,16 @@
 <body>
     <header class="header"> <?php include 'include/header.php'; ?></header>
 
-    <div class="main-wrapper">
-        <aside class="sidebar">
-            <?php include 'include/dashboard/sidebar.php'; ?>
-        </aside>
-
-        <main class="parent">
-            <div class="temperature"><?php include 'include/dashboard/buiten-temp.php'; ?></div>
-            <div class="sunsetsunrise"><?php include 'include/dashboard/sunset-sunrise.php'; ?></div>
-            <div class="weatherforecast"><?php include 'include/dashboard/weatherforecast.php'; ?></div>
-            <div class="zonnepanelen"><?php include 'include/dashboard/zonnepanelen.php'; ?></div>
-            <div class="energie"><?php include 'include/dashboard/energie.php'; ?></div>
-            <div class="lampen"><?php include 'include/dashboard/lampen.php'; ?></div>
-            <div class="parent"></div>
-            </main>
-
-    </div>
+    <?php include 'include/dashboard/sidebar.php'; ?>
+    <?php include 'include/dashboard/buiten-temp.php'; ?>
+    <?php include 'include/dashboard/sunset-sunrise.php'; ?>
+    <?php include 'include/dashboard/weatherforecast.php'; ?>
+    <?php include 'include/dashboard/zonnepanelen.php'; ?>
+    <?php include 'include/dashboard/energie.php'; ?>
+    <?php include 'include/dashboard/lampen.php'; ?>
     
-    <footer class="footer"><?php include 'include/footer.php'; ?></div>
 </body> 
+<footer class="footer"><?php include 'include/footer.php'; ?></div>
 </html>
 
 
