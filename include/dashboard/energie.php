54a87663ae5f27e0fe8db8a907899df82f7ce056
DEON <40478@ma-web.nl>
2026-06-12 09:17:58 +0200

    del grid

--- include/dashboard/energie.php
+++ include/dashboard/energie.php
@@ -1,1 +1,33 @@
 <p>Energie verbruik</p>
+
+
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function loadAndDrawChart() {
+        fetch('include/gas.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Netwerk fout');
+                }
+                return response.json();
+            })
+            .then(jsonData => {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Naam');
+    data.addColumn('number', 'Waarde');
+
+    jsonData.forEach(item => {
+        data.addRow([item.naam, item.verbruik]);
+        console.log(`toegevoegd: ${item.datum} - ${item.verbruik}`);
+    });
+
+    var options = {'title': 'gasverbruik'};
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+    })
+    .catch(error => console.error('Fout bij het laden data:', error));
+}
+
+</script>
