e33c5dd04383528682492e0a8c0454d43a6781a3
Ian <40573@ma-web.nl>
2026-06-15 11:36:45 +0200

    fix layout

--- index.php
+++ index.php
@@ -3,34 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard-Duurzaam</title>
-    <link rel="stylesheet" href="css/style.css">
-
+    <title>Document</title>
+  <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
-
-<div class="page-container">
-    <header><?php include 'include/header.php'; ?></header>
-
-    <div class="main-wrapper">
-        <aside class="sidebar">
-            <?php include 'include/dashboard/sidebar.php'; ?>
-        </aside>
-
-        <main class="kyano-balaap">
-            <div class="temperature"><?php include 'include/dashboard/buiten-temp.php'; ?></div>
-            <div class="sunsetsunrise"><?php include 'include/dashboard/sunset-sunrise.php'; ?></div>
-            <div class="weatherforecast"><?php include 'include/dashboard/weatherforecast.php'; ?></div>
-            <div class="zonnepanelen"><?php include 'include/dashboard/zonnepanelen.php'; ?></div>
-            <div class="energie"><?php include 'include/dashboard/energie.php'; ?></div>
-            <div class="lampen"><?php include 'include/dashboard/lampen.php'; ?></div>
-            <div class="parent"></div>
-            </main>
-
-    </div>
-    
-    <footer class="footer"><?php include 'include/footer.php'; ?></div>
-</body> 
-<footer class="footer"><?php include 'include/footer.php'; ?></div>
+    <?php include 'Layout(H-M-F)/header.php'; ?>
+    <?php include 'Layout(H-M-F)/main.php'; ?>
+    <?php include 'Layout(H-M-F)/footer.php'; ?>
+</body>
 </html>
-
---
+++ main.php
@@ -0,0 +1,19 @@
+<main>
+    <aside class="sidebar">
+        <h1>Dashboard</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Time</li>
+                <li>Temperature</li>
+                <li>Weather</li>
+                <li>Dawn & Dusk</li>
+                <li>Sunpanels</li>
+            </ul>
+        </nav>
+    </aside>
+    <section class="dashbaord-content">
+<?php include 'Dashboard-include/Weather.php'; ?>
+<?php include 'Dashboard-include/Sun-energie.php';?>
+    </section>
+</main>
