47bc078ee17edf8b06c7aad4da33a140b3992eed
Ian <40573@ma-web.nl>
2026-06-15 11:55:09 +0200

    darkmode deleted for now (caused problems)

--- include/Dashboard-include/Darkmode.php
+++ include/Dashboard-include/Darkmode.php
@@ -1,26 +1,2 @@
-<script>
-    function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
-</script>
-
-<style>
-
-.dark-mode {
-  background-color: black;
-  color: white;
-}
-
-button {
-    background-color:black;
-    color: white;
-    border-radius: 7px;
-    height: 30px;
-    width: 100px;
-}
-</style>
-
-<button onclick="myFunction()">Toggle dark mode</button>
 
 
