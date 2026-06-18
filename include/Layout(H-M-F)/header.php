8b104e1571d02410da6d6ff10cdf8ca71ccc81c3
Kyano <40870@ma-web.nl>
2026-06-18 12:21:33 +0200

    logo add

--- include/Layout(H-M-F)/header.php
+++ include/Layout(H-M-F)/header.php
@@ -1,9 +1,8 @@
 <header>
+    <img src="img/logo.png" alt="EcoTrack Logo" class="logo">
     <p>EcoTrack</p>
     <div class="DarkIan">
     <?php include 'include/Dashboard-include/Darkmode.php';?>
     <p>Ian</p>
 </div>
 </header>
-
-
